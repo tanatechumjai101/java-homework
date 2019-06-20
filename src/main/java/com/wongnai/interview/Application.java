dbf53ebf81cf5c45a06efada5250f0ae340367c6
Tanate <tanate.chu.git@gmail.com>
2019-06-20 22:40:51 +0700

    [Added] Add WebServerFactoryCustomizer set new Port.

--- src/main/java/com/wongnai/interview/Application.java
+++ src/main/java/com/wongnai/interview/Application.java
@@ -1,13 +1,23 @@
 package com.wongnai.interview;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.server.ConfigurableWebServerFactory;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableJpaRepositories
-public class Application {
-	public static void main(String[] args) { SpringApplication.run(Application.class, args);
+//@ComponentScan
+public class Application implements WebServerFactoryCustomizer<ConfigurableWebServerFactory> {
 
+	public static void main(String[] args) {
+
+		SpringApplication.run(Application.class, args);
+
+	}
+
+	@Override
+	public void customize(ConfigurableWebServerFactory factory) {
+		factory.setPort(8085);
 	}
 }
